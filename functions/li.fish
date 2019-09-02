5649574fde8653fa3d4c557a6a20ea69faf9809e
Nathan DeGruchy <nathan@degruchy.org>
2019-09-02 13:42:41 -0400

    New alias

---
+++ functions/li.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function li --description 'alias li exa --group-directories-first --long --sort=Name --header --git --binary'
+	exa --group-directories-first --long --sort=Name --header --git --binary $argv;
+end
