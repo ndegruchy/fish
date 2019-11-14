f637ac49cd9a22fb2ed87f5fa871066dc8215a04
Nathan DeGruchy <nathan@degruchy.org>
2019-11-14 15:40:48 -0500

    Cleanup

--- functions/feh.fish
+++ functions/feh.fish
@@ -1,8 +1,8 @@
 function feh -d "Feh with some default options"
     if [ -x /usr/bin/feh ]
-	/usr/bin/feh --auto-zoom --auto-rotate --draw-filename --draw-tinted --fullscreen --borderless --geometry 1920x1080 --image-bg black $argv
+	/usr/bin/feh --auto-zoom --auto-rotate --draw-filename --draw-tinted --fullscreen --borderless --image-bg black $argv
     else
 	echo "Feh doesn't seem to be installed."
-	exit 1
+	return 1
     end
 end
