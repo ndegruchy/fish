9877f626cbdb6e389e24e09d134a2f2439a7b94c
Nathan DeGruchy <nathan@degruchy.org>
2019-11-26 20:21:24 -0500

    Updates

--- functions/feh.fish
+++ functions/feh.fish
@@ -1,6 +1,6 @@
 function feh -d "Feh with some default options"
     if [ -x /usr/bin/feh ]
-	/usr/bin/feh --auto-zoom --auto-rotate --draw-filename --draw-tinted --fullscreen --borderless --image-bg black $argv
+	/usr/bin/feh --auto-zoom --auto-rotate --draw-filename --draw-tinted --fullscreen --borderless --image-bg black --scale-down --start-at $argv
     else
 	echo "Feh doesn't seem to be installed."
 	return 1
--- functions/ll.fish
+++ functions/ll.fish
@@ -2,6 +2,6 @@ function ll -d "Use exa to display long formatted list of files"
     if [ -x /usr/bin/exa ]
 	exa --long --all --header --time-style=long-iso --git --group-directories-first --sort=Name $argv
     else
-	ls -alh
+	ls -alh --group-directories-first
     end
 end
