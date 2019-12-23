8bcc7b3f0a443562282d204e994636582c9c1aa4
Nathan DeGruchy <nathan@degruchy.org>
2019-12-23 11:13:07 -0500

    Remove base16

--- config.fish
+++ config.fish
@@ -144,9 +144,3 @@ if status --is-interactive
         end
     end
 end
-
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
